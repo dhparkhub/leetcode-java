70e629fbc88f6980256a6a4872498c9197348e55
dhpark <dhpark0648@gmail.com>
2020-04-05 00:54:50 +0900

    solve 1389

---
+++ src/solved/P1389.java
@@ -0,0 +1,23 @@
+package solved;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class P1389 {
+
+    class Solution {
+        public int sumFourDivisors(int[] nums) {
+
+        }
+
+        void init() {
+            primes = new ArrayList<>();
+            
+        }
+
+        private List<Integer> primes;
+        private Map<Integer, Boolean> productOfTwoPrimes;
+    }
+
+}
