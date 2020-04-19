8dcad8ed9fadc4873e6e8aab44d2188d93b3b939
dhpark <dhpark0648@gmail.com>
2020-04-19 15:35:15 +0900

    solve 7

---
+++ src/solved/P7.java
@@ -0,0 +1,16 @@
+package solved;
+
+public class P7 {
+
+    class Solution {
+        public int reverse(int x) {
+            long answer = 0;
+            while (x != 0) {
+                answer = answer * 10 + x % 10;
+                x /= 10;
+            }
+            return answer == (int) answer ? (int) answer : 0;// prevent overflow
+        }
+    }
+
+}
