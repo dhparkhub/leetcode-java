4dd60fbdb26161c84de6fe3d5d9a4466ddedef44
dhpark <dhpark0648@gmail.com>
2020-04-20 21:37:42 +0900

    solve weekly contest 184

---
+++ src/solved/p1408/Solution.java
@@ -0,0 +1,26 @@
+package solved.p1408;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> answer = new ArrayList<>();
+        Map<String, Boolean> map = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            boolean flag = true;
+            for (int j = 0; j < words.length; j++) {
+                if (i == j) continue;
+                if (words[i].contains(words[j])) {
+                    if (!map.containsKey(words[j])) {
+                        answer.add(words[j]);
+                        map.put(words[j], true);
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
---
+++ src/solved/p1409/Solution.java
@@ -0,0 +1,27 @@
+package solved.p1409;
+
+class Solution {
+    public int[] processQueries(int[] queries, int m) {
+        int[] arr = new int[m];
+        for (int i = 0; i < m; i++) {
+            arr[i] = i + 1;
+        }
+
+        int[] answer = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[j] == queries[i]) {
+                    answer[i] = j;
+                    for (int k = j; k > 0; k--) {
+                        int temp = arr[k];
+                        arr[k] = arr[k - 1];
+                        arr[k - 1] = temp;
+                    }
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
