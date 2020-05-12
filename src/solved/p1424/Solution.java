27d3f1edeb0a7cab0b82e82d0aeb6df1653693f9
Daeho Park <dhpark0648@gmail.com>
2020-05-12 15:01:16 +0900

    solve 1424

---
+++ src/solved/p1424/Solution.java
@@ -0,0 +1,33 @@
+package solved.p1424;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+class Solution {
+    private final Map<Integer, LinkedList<Integer>> map = new TreeMap<>();
+
+    public int[] findDiagonalOrder(List<List<Integer>> nums) {
+        int totalSize = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            totalSize += nums.get(i).size();
+            for (int j = 0; j < nums.get(i).size(); j++) {
+                if (!map.containsKey(i + j)) {
+                    map.put(i + j, new LinkedList<>());
+                }
+                map.get(i + j).addFirst(nums.get(i).get(j));
+            }
+        }
+
+        int[] answer = new int[totalSize];
+        int index = 0;
+        for (int key : map.keySet()) {
+            for (int i : map.get(key)) {
+                answer[index++] = i;
+            }
+        }
+
+        return answer;
+    }
+}
