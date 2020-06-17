656b7177923448a0afea4b37996f4d418175cb84
Daeho Park <dhpark0648@gmail.com>
2020-06-17 15:08:22 +0900

    [solve] 38. Count and Say

---
+++ src/p38/Solution.java
@@ -0,0 +1,20 @@
+package p38;
+
+class Solution {
+    public String countAndSay(int n) {
+        if (n == 1) return "1";
+        String s = countAndSay(n - 1);
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (i < s.length()) {
+            int k = 1;
+            while (i + 1 < s.length() && s.charAt(i) == s.charAt(i + 1)) {
+                i++;
+                k++;
+            }
+            sb.append(String.format("%d%c", k, s.charAt(i)));
+            i++;
+        }
+        return sb.toString();
+    }
+}
