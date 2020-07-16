6e03e7f347953bbecf7afb432e625804b5037c91
Daeho Park <danny.park@lgcns.com>
2020-07-16 17:19:22 +0900

    [solve] 101. Symmetric Tree

---
+++ src/p101/Solution.java
@@ -0,0 +1,75 @@
+package p101;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        List<Integer> lList = preorder(root.left);
+        List<Integer> rList = postorder(root.right);
+        return lList.equals(rList);
+    }
+
+    private List<Integer> preorder(TreeNode node) {
+        List<Integer> list = new ArrayList<>();
+        if (node == null) {
+            return list;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(node);
+        while (!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            if (temp == null) {
+                list.add(null);
+                continue;
+            }
+            list.add(temp.val);
+            stack.push(temp.left);
+            stack.push(temp.right);
+        }
+        return list;
+    }
+
+    private List<Integer> postorder(TreeNode node) {
+        List<Integer> list = new ArrayList<>();
+        if (node == null) {
+            return list;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(node);
+        while (!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            if (temp == null) {
+                list.add(null);
+                continue;
+            }
+            list.add(temp.val);
+            stack.push(temp.right);
+            stack.push(temp.left);
+        }
+        return list;
+    }
+}
