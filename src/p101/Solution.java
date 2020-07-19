3dcac9ff33b8a6226d21be1a5f08ce4203b9bf66
Daeho Park <dhpark0648@gmail.com>
2020-07-19 11:14:17 +0900

    [solve] 101. Symmetric Tree

--- src/p101/Solution.java
+++ src/p101/Solution.java
@@ -1,9 +1,5 @@
 package p101;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
-
 class TreeNode {
     int val;
     TreeNode left;
@@ -25,51 +21,90 @@ class TreeNode {
 
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null) {
+        return root == null ? true : isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
             return true;
         }
-        List<Integer> lList = preorder(root.left);
-        List<Integer> rList = postorder(root.right);
-        return lList.equals(rList);
-    }
-
-    private List<Integer> preorder(TreeNode node) {
-        List<Integer> list = new ArrayList<>();
-        if (node == null) {
-            return list;
+        if (left == null || right == null) {
+            return false;
         }
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(node);
-        while (!stack.isEmpty()) {
-            TreeNode temp = stack.pop();
-            if (temp == null) {
-                list.add(null);
-                continue;
-            }
-            list.add(temp.val);
-            stack.push(temp.left);
-            stack.push(temp.right);
-        }
-        return list;
-    }
-
-    private List<Integer> postorder(TreeNode node) {
-        List<Integer> list = new ArrayList<>();
-        if (node == null) {
-            return list;
-        }
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(node);
-        while (!stack.isEmpty()) {
-            TreeNode temp = stack.pop();
-            if (temp == null) {
-                list.add(null);
-                continue;
-            }
-            list.add(temp.val);
-            stack.push(temp.right);
-            stack.push(temp.left);
-        }
-        return list;
+        return (left.val == right.val) && isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
     }
 }
+
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Stack;
+//
+//class TreeNode {
+//    int val;
+//    TreeNode left;
+//    TreeNode right;
+//
+//    TreeNode() {
+//    }
+//
+//    TreeNode(int val) {
+//        this.val = val;
+//    }
+//
+//    TreeNode(int val, TreeNode left, TreeNode right) {
+//        this.val = val;
+//        this.left = left;
+//        this.right = right;
+//    }
+//}
+//
+//class Solution {
+//    public boolean isSymmetric(TreeNode root) {
+//        if (root == null) {
+//            return true;
+//        }
+//        List<Integer> lList = preorder(root.left);
+//        List<Integer> rList = postorder(root.right);
+//        return lList.equals(rList);
+//    }
+//
+//    private List<Integer> preorder(TreeNode node) {
+//        List<Integer> list = new ArrayList<>();
+//        if (node == null) {
+//            return list;
+//        }
+//        Stack<TreeNode> stack = new Stack<>();
+//        stack.push(node);
+//        while (!stack.isEmpty()) {
+//            TreeNode temp = stack.pop();
+//            if (temp == null) {
+//                list.add(null);
+//                continue;
+//            }
+//            list.add(temp.val);
+//            stack.push(temp.left);
+//            stack.push(temp.right);
+//        }
+//        return list;
+//    }
+//
+//    private List<Integer> postorder(TreeNode node) {
+//        List<Integer> list = new ArrayList<>();
+//        if (node == null) {
+//            return list;
+//        }
+//        Stack<TreeNode> stack = new Stack<>();
+//        stack.push(node);
+//        while (!stack.isEmpty()) {
+//            TreeNode temp = stack.pop();
+//            if (temp == null) {
+//                list.add(null);
+//                continue;
+//            }
+//            list.add(temp.val);
+//            stack.push(temp.right);
+//            stack.push(temp.left);
+//        }
+//        return list;
+//    }
+//}
